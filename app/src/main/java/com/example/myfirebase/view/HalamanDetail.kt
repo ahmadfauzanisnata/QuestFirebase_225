69ad0b957c647737b9db9037da987551570f305e
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2026-01-07 22:31:49 +0700

    tambah composable DetailSiswaScreen dan inisialisasi load data detail

---
+++ app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
@@ -0,0 +1,28 @@
+package com.example.myfirebase.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myfirebase.R
+import com.example.myfirebase.view.route.DestinasiDetail
+import com.example.myfirebase.viewmodel.DetailViewModel
+import com.example.myfirebase.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    idSiswa: Long,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+    LaunchedEffect(key1 = idSiswa) {
+        viewModel.loadDetail(idSiswa)
+    }
+
